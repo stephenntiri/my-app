73943aa758d7cf77d5c07e78461394ce9eb38656
root <root@jenkins.internal>
2018-05-31 17:04:31 +0000

    Changed feature2 title to merge feature2 to integration

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is Hello World! feature2 application" );
+        System.out.println( "Merge feature2 branch to integration" );
     }
 }
